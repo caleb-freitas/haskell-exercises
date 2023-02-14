5c3bfaf44ac07b6ea03a4556515127384eba50c2
Caleb Freitas <calebfreitas@tutanota.com>
2023-02-14 18:59:22 -0300

    feat: added iterative solver

--- src/DeclaringTypesAndClasses.hs
+++ src/DeclaringTypesAndClasses.hs
@@ -1,3 +1,25 @@
-safehead :: [a] -> Maybe a
-safehead [] = Nothing
-safehead xs = Just (head xs)
+module DeclaringTypesAndClasses () where
+
+-- | Exercise 1
+data Nat = Zero | Succ Nat
+
+add :: Nat -> Nat -> Nat
+add Zero n = n
+add (Succ m) n = Succ (add m n)
+
+mult :: Nat -> Nat -> Nat
+mult m Zero = Zero
+mult m (Succ n) = add m (mult m n)
+
+{-
+Multiplication of 2 * 1:
+
+ghci> mult (Succ (Succ Zero)) (Succ Zero)
+
+Evaluation:
+
+add (Succ (Succ Zero)) (mult (Succ (Succ Zero))  Zero)
+add (Succ (Succ Zero)) (Zero)
+Succ (Succ Zero)
+-}
+
--- src/SudokuSolver.hs
+++ src/SudokuSolver.hs
@@ -1,6 +1,6 @@
 module SudokuSolver () where
 
-import Data.List (transpose)
+import Data.List (transpose, (\\))
 
 -- | Basic declarations
 type Value = Char
@@ -22,6 +22,20 @@ single :: [a] -> Bool
 single [_] = True
 single _ = False
 
+-- | Examples grids
+easy =
+    [
+        "2....1.38",
+        "........5",
+        ".7...6...",
+        ".......13",
+        ".981..257",
+        "31....8..",
+        "9..8...2.",
+        ".5..69784",
+        "4..25...."
+    ]
+
 -- | Extract rows from a matrix.
 rows :: Matrix a -> [Row a]
 rows = id
@@ -42,12 +56,65 @@ chop :: Int -> [a] -> [[a]]
 chop n [] = []
 chop n xs = take n xs : chop n (drop n xs)
 
+-- | Checks for a valid grid (following the three basic rules of sudoku).
 valid :: Grid -> Bool
 valid g =
     all nodups (rows g)
         && all nodups (cols g)
         && all nodups (boxes g)
 
+-- | Checks for duplicated values in a list.
 nodups :: Eq a => [a] -> Bool
 nodups [] = True 
 nodups (x:xs) = notElem x xs && nodups xs
+
+-- | Basic solver using brute force (takes too long).
+bruteForceSolver :: Grid -> [Grid]
+bruteForceSolver = filter valid . explode . choices
+
+-- | Replaces each blank cell by all of possible number between 1 and 9.
+type Choices = [Value]
+choices :: Grid -> Matrix Choices
+choices = map (map choice)
+    where
+        choice v = if v == '.' then ['1'..'9'] else [v]
+
+-- | Explode a matrix of choices to a choice of matrices by reducing all possible
+-- combinations of choices for each cell.
+explode :: Matrix [a] -> [Matrix a]
+explode m = cartesianProduct (map cartesianProduct m)
+
+-- | Calculate all the possible combinations of a list of lists values.
+cartesianProduct :: [[a]] -> [[a]]
+cartesianProduct [] = [[]]
+cartesianProduct (xs:xss) = [k:ks | k <- xs, ks <- cartesianProduct xss]
+
+-- | Prunes the search space for invalid choices.
+prune :: Matrix Choices -> Matrix Choices
+prune = pruneBy boxes . pruneBy cols . pruneBy rows
+    where
+        pruneBy f = f . map reduce . f
+
+-- | Eliminate choices which have no possibility to ever suceed.
+reduce :: Row Choices -> Row Choices
+reduce xss = [xs `minus` singles | xs <- xss] 
+    where
+        singles = concat (filter single xss)
+
+minus :: Choices -> Choices -> Choices
+xs `minus` ys = if single xs then xs else xs \\ ys
+
+-- | Improved solver using the prune helper function (still takes too long).
+pruneSolver :: Grid -> [Grid]
+pruneSolver = filter valid . explode . prune . choices
+
+-- | A better solver using iterative function fix to get the fixed pointe in the
+-- list of choices.
+iterativeSolver :: Grid -> [Grid]
+iterativeSolver = filter valid . explode . fix prune . choices
+
+-- | Get the fixed-point of a list iteractively.
+fix :: Eq a => (a -> a) -> a -> a
+fix f x = if x == x' then x else fix f x'
+    where
+        x' = f x
